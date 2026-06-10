ca433a2c78b26d62de296e9168bd20eb9ed80690
mgreen1999 <mari90tc@stud.kea.dk>
2026-06-10 11:03:14 +0200

    footer rettet

--- src/components/(G_FooterComponents)/FooterPost.jsx
+++ src/components/(G_FooterComponents)/FooterPost.jsx
@@ -5,8 +5,8 @@ const FooterPost = () => {
   return (
     <div>
       <p className="mb-14 3xl:text-base font-bold uppercase tracking-[0.3em] text-nightclub-pink">Recent Posts</p>
-      <FooterPostCards pic={<Image src="/contentImg/recent_post1.jpg" alt="post1" width={80} height={80} />} text={"Lorem Ipsum is simply dummy text of the printing and typesetting."} date={"April 17, 2018"} />
-      <FooterPostCards pic={<Image src="/contentImg/recent_post2.jpg" alt="post2" width={80} height={80} />} text={"Lorem Ipsum is simply dummy text of the printing and typesetting."} date={"April 17, 2018"} />
+      <FooterPostCards pic={<Image src="/contentImg/recent_post1.jpg" alt="post1" width={80} height={80} />} text={"Get ready for an epic night with DJ Anya at the The Blue Room. Doors open at 9 PM!"} date={"April 17, 2018"} />
+      <FooterPostCards pic={<Image src="/contentImg/recent_post2.jpg" alt="post2" width={80} height={80} />} text={"Join us for a Retro Night with DJ Elroy at The Velvet Room. Doors open at 8 PM!"} date={"April 17, 2018"} />
     </div>
   );
 };
